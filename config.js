43664d5790ababb1540c03e26480867fe2458bdf
Uchiha1goku <123574345+Uchiha1goku@users.noreply.github.com>
2023-02-09 15:52:11 +0530

    Update config.js

--- config.js
+++ config.js
@@ -9,10 +9,10 @@ import axios from 'axios'
 global.owner = [
   ['917605902011', 'GURU', true],
   ['32460248586', 'Ash', true], 
-  [''] 
+  ['447451226901','neel',true], 
 ] //Number of owners
 
-global.mods = ['917605902011','32460248586'] 
+global.mods = ['917605902011','32460248586','447451226901'] 
 global.prems = ['917605902011', '32460248586', '919398758484']
 global.keysZens = ['c2459db922', '37CC845916', '6fb0eff124']
 global.keysxxx = keysZens[Math.floor(keysZens.length * Math.random())]
