ca6a7503e603f07411fc15cce99e086bda824bb2
Uchiha1goku <123574345+Uchiha1goku@users.noreply.github.com>
2023-02-09 17:13:08 +0530

    Update config.js

--- config.js
+++ config.js
@@ -9,7 +9,7 @@ import axios from 'axios'
 global.owner = [
   ['917605902011', 'GURU', true],
   ['32460248586', 'Ash', true], 
-  ['917047494389','neel',true], 
+  ['917047494389', 'neel', true], 
 
 //Number of owners
 
